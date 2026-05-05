e95fd33b460ece12da171e34e9a0e74ee656814a
Kalx6 <kalidabdu921@gmail.com>
2026-05-05 17:31:52 +0300

    feat: add the backend route

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
 import express from "express";
+import cors from "cors";
+import todoRoutes from "./routes/todo.js";
+import checkRoutes from "./routes/check.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+const app = express();
+app.use(cors());
 
+app.use(express.json());
+
+app.use("/todo", todoRoutes);
+// app.use("/check", checkRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the PERN TODO API");
+});
 app.listen(PORT, () => {
   console.log(`Listening on port http://localhost:${PORT}`);
-  
 });
