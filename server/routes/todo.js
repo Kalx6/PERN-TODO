4719a772748be8af9b487e5655d848ee751412de
Kalx6 <kalidabdu921@gmail.com>
2026-05-08 21:41:01 +0300

    add router to update,delete and get todo

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -3,6 +3,8 @@ import pool from "../db.js";
 
 const router = Router();
 
+// Add todo
+
 router.post("/", async (req, res) => {
   try {
     console.log(req.body);
@@ -18,4 +20,48 @@ router.post("/", async (req, res) => {
   }
 });
 
+// get todo
+
+router.get("/", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("server Error");
+  }
+});
+
+// Update todo
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description, completed } = req.body;
+
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1, completed = $2 WHERE todo_id = $3 RETURNING *",
+      [description, completed, id],
+    );
+    res.json(updateTodo.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server failure");
+  }
+});
+
+// Delete
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.send("Task deleted");
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server Error");
+  }
+});
 export default router;
