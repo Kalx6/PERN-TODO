66f40f774765ae5b207e94b930def988c3c9eed7
Kalx6 <kalidabdu921@gmail.com>
2026-05-09 14:49:41 +0300

    setup the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useState } from "react";
 
 function App() {
+  const [description, setDescription] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
+  const [editedTodo, setEditedTodo] = useState("");
+
   return (
-    <div>App</div>
-  )
+    <div className="min-h-screen bg-gray-800 flex justify-center items-center p-4">
+      <div className="bg-gray-50 rounded-2xl shadow-2xl w-full max-w-lg p-8">
+        <h1 className="text-4xl text-center font-bold text-gray-800">
+          PERN TODO APP
+        </h1>
+        <form action="">
+          <input
+            className="w-full outline-none px-3 [y-2 text-gray-700 placeholder-gray-400 mt-4 transition-all focus:border-b-[1px] focus:border-gray-400 "
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="What needs to be done?"
+          />
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
